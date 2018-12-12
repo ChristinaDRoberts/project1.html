17c8ea2237c67b32072fda515c9a6e73fca05c94
Christina <Christinepeninsula@hotmail.com>
2018-12-12 13:08:32 -0500

    layout changes
    changed div and section layouts, changed link colors, changed spacing for tiopics section

--- ex2.js
+++ ex2.js
@@ -1,1 +1,63 @@
 //excersize2.js
+// Hey Carolina Code School Hackers! Enjoy!
+// Make sure to open your js consoles!
+
+// NUMBER 1: REFACTOR addNumbers
+
+// The below function does not work. The variable twoPlusTwo gets set to undefined.
+// Refactor the function to make it work.
+
+function addNumbers(numberA, numberB) {
+
+  return (numberA + numberB);
+  var twoPlusTwo;
+  twoPlusTwo = addNumbers(2, 2);
+  return twoPlusTwo
+}
+
+
+
+
+// Number 2: yell()
+// Write a function named yell that returns an uppercase version of a string
+
+function yell(a){
+  return a.toUpperCase();
+}
+
+
+
+
+
+
+
+
+
+// Number 3: yell10()
+// Write a function called yell10 that uses your yell function to
+// log out an uppercase version of a string 10 times.
+
+
+
+
+
+
+
+
+
+
+// Number 4: longest()
+
+
+
+
+
+
+
+
+
+
+// Number 5: isVowel()
+// Write a function called isVowel that takes a character (i.e. a string of length 1)
+// and returns true if it is a vowel, uppercase or lowercase. The function should return
+// false if the character is not a vowel.
